1137faafbc46bfe59b8aff1b1a966efbb7a2fe12
krassowski <krassowski.michal@gmail.com>
2016-05-03 15:48:56 +0200

    Added inline documentation

--- cna_to_tabix.sh
+++ cna_to_tabix.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-((head -n 1 $1 | sed 's/Chromosome:G_Start\.\.G_Stop/Chromosome\tG_Start\tG_Stop/') && (tail -n +2 $1 | sed -E 's/(.):([0-9]+)\.\./\1\t\2\t/' | sort -k20,20 -k21,21n -k22,22n)) | bgzip > $1.tabix.gz;
-tabix -s 20 -b 21 -e 22 -f -S 1 $1.tabix.gz;
-# -S 1 = skip first line (header)
-# -s 20 = chromosome column
-# -f = overwrite existing file if any
+(
+    (
+        head -n 1 $1 |  # take care of header
+        sed 's/Chromosome:G_Start\.\.G_Stop/Chromosome\tG_Start\tG_Stop/'   # split column names for chromosomal position
+    ) &&
+    (
+        tail -n +2 $1 | # take care for all lines after header
+        sed -E 's/(.):([0-9]+)\.\./\1\t\2\t/' | # replace Y:12..15 to Y  12   15
+        sort -t $'\t' -k20,20 -k21,21n -k22,22n # sort the stuff: use tab as sep (crucial!), 20 - chrom name, 21 - start, 22 - end
+    )
+) | bgzip > $1.tabix.gz;    # block gzip with a tool from htslib
+
+tabix -s 20 -b 21 -e 22 -f -S 1 $1.tabix.gz;    # create index with tabix from htslib: -S 1 = skip first line (header)
+                                                # -s 20 = chrom, -b 21 = start, -e 22 = end, -f = update old file if exists
