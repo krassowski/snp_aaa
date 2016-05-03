598f9eac868befd7481359878cd00091643d9d2b
krassowski <krassowski.michal@gmail.com>
2016-05-03 19:19:19 +0200

    Updated tabix conversion scripts

--- cna_to_tabix.sh
+++ cna_to_tabix.sh
@@ -9,7 +9,7 @@
         sed -E 's/(.):([0-9]+)\.\./\1\t\2\t/' | # replace Y:12..15 to Y  12   15
         sort -t $'\t' -k20,20 -k21,21n -k22,22n # sort the stuff: use tab as sep (crucial!), 20 - chrom name, 21 - start, 22 - end
     )
-) | bgzip > $1.tabix.gz;    # block gzip with a tool from htslib
+) | bgzip > $1.sorted.gz;    # block gzip with a tool from htslib
 
-tabix -s 20 -b 21 -e 22 -f -S 1 $1.tabix.gz;    # create index with tabix from htslib: -S 1 = skip first line (header)
-                                                # -s 20 = chrom, -b 21 = start, -e 22 = end, -f = update old file if exists
+tabix -s 20 -b 21 -e 22 -f -S 1 $1.sorted.gz;    # create index with tabix from htslib: -S 1 = skip first line (header)
+                                                 # -s 20 = chrom, -b 21 = start, -e 22 = end, -f = update old file if exists
--- vcf_to_tabix.sh
+++ vcf_to_tabix.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# bgzip and tabix comes from htslib library
 filename=$1
 bgzip -c $filename > $filename.gz
 tabix -p vcf $filename.gz
