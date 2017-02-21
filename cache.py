8efce3ffd12c8e17f745fa138174b202096e70ab
krassowski <krassowski.michal@gmail.com>
2017-02-21 17:30:13 +0100

    Give a hint if cache load error occurs

--- cache.py
+++ cache.py
@@ -2,6 +2,10 @@ from __future__ import print_function
 import cPickle as pickle
 
 
+class CachingError(Exception):
+    pass
+
+
 def cached(action='load'):
     """Makes caching a pleasure. Use as a decorator."""
     def decorator(generating_function):
@@ -11,7 +15,16 @@ def cached(action='load'):
 
             if action == 'load':
                 with open(cache_name, 'rb') as f:
-                    variable = pickle.load(f)
+                    try:
+                        variable = pickle.load(f)
+                    except AttributeError as e:
+                        raise CachingError(
+                            'Unable to load cache for %s. '
+                            'Please, make sure that the cache files correspond'
+                            ' to this version of application.\n'
+                            'Oryginal exception: %s.' %
+                            (display_name, e.message)
+                        )
                 # print('"' + display_name + '" data loaded from cache')
             else:
                 print('Generating "' + display_name + '"...')
