68d79748534a2830b2a7c59cb1cec69eaa2c7ede
krassowski <krassowski.michal@gmail.com>
2017-05-12 14:43:04 +0200

    Update sequence fetching to new ensembl urls

--- download_ensembl.sh
+++ download_ensembl.sh
@@ -8,11 +8,11 @@ ensembl_version=88
 assembly=37
 
 cd ensembl
-if [ -d "v$ensembl_version" ]
+if [ -d "av$ensembl_version" ]
 then
     echo "Ensembl data from $ensembl_version release are already downloaded"
 else
-    mkdir "v$ensembl_version"
+    mkdir -p "v$ensembl_version"
     cd "v$ensembl_version"
 
     if (( $ensembl_version < 76 ))
@@ -20,8 +20,8 @@ else
         assembly="$assembly.$ensembl_version"
     fi
 
-    wget ftp://ftp.ensembl.org/pub/release-$ensembl_version/fasta/homo_sapiens/cds/Homo_sapiens.GRCh$assembly.cds.all.fa.gz
-    wget ftp://ftp.ensembl.org/pub/release-$ensembl_version/fasta/homo_sapiens/cdna/Homo_sapiens.GRCh$assembly.cdna.all.fa.gz
+    wget ftp://ftp.ensembl.org/pub/grch$assembly/release-$ensembl_version/fasta/homo_sapiens/cds/Homo_sapiens.GRCh$assembly.cds.all.fa.gz
+    wget ftp://ftp.ensembl.org/pub/grch$assembly/release-$ensembl_version/fasta/homo_sapiens/cdna/Homo_sapiens.GRCh$assembly.cdna.all.fa.gz
     # download chromosomes, without pathes
 
     if (( $assembly > 37 ))
@@ -29,8 +29,8 @@ else
         wget ftp://ftp.ensembl.org/pub/release-$ensembl_version/variation/vcf/homo_sapiens/Homo_sapiens.vcf.gz
         wget ftp://ftp.ensembl.org/pub/release-$ensembl_version/variation/vcf/homo_sapiens/Homo_sapiens.vcf.gz.tbi
     else
-        wget ftp://ftp.ensembl.org/pub/grch$assembly/update/variation/vcf/homo_sapiens/Homo_sapiens.vcf.gz
-        wget ftp://ftp.ensembl.org/pub/grch$assembly/update/variation/vcf/homo_sapiens/Homo_sapiens.vcf.gz.tbi
+        wget ftp://ftp.ensembl.org/pub/grch$assembly/release-$ensembl_version/variation/vcf/homo_sapiens/Homo_sapiens.vcf.gz
+        wget ftp://ftp.ensembl.org/pub/grch$assembly/release-$ensembl_version/variation/vcf/homo_sapiens/Homo_sapiens.vcf.gz.tbi
         #if (( $ensembl_version < 76 ))
         #then
         #    echo "Indexing vcf.gz file ..."
@@ -42,12 +42,12 @@ else
     fi
 
 
-    wget ftp://ftp.ensembl.org/pub/release-$ensembl_version/fasta/homo_sapiens/dna/Homo_sapiens.GRCh$assembly.dna.chromosome.X.fa.gz
-    wget ftp://ftp.ensembl.org/pub/release-$ensembl_version/fasta/homo_sapiens/dna/Homo_sapiens.GRCh$assembly.dna.chromosome.Y.fa.gz
-    wget ftp://ftp.ensembl.org/pub/release-$ensembl_version/fasta/homo_sapiens/dna/Homo_sapiens.GRCh$assembly.dna.chromosome.*.fa.gz -R Homo_sapiens.GRCh$assembly.dna.chromosome.H*.fa.gz
+    #wget ftp://ftp.ensembl.org/pub/grch$assembly/release-$ensembl_version/fasta/homo_sapiens/dna/Homo_sapiens.GRCh$assembly.dna.chromosome.X.fa.gz
+    #wget ftp://ftp.ensembl.org/pub/grch$assembly/release-$ensembl_version/fasta/homo_sapiens/dna/Homo_sapiens.GRCh$assembly.dna.chromosome.Y.fa.gz
+    wget "ftp://ftp.ensembl.org/pub/grch$assembly/release-$ensembl_version/fasta/homo_sapiens/dna/Homo_sapiens.GRCh$assembly.dna.chromosome.*.fa.gz" -R "Homo_sapiens.GRCh$assembly.dna.chromosome.H*.fa.gz"
 fi
 
-
+shopt -s nullglob
 for i in *.fa.gz;
 do
 	echo "Recompressing $i";
