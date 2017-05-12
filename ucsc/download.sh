cc45a8a549ab051b0b09f21b03475ee6e8e7400a
krassowski <krassowski.michal@gmail.com>
2017-05-12 11:57:18 +0200

    Fix filename for sequences download

--- ucsc/download.sh
+++ ucsc/download.sh
@@ -3,5 +3,5 @@ wget http://genome.ucsc.edu/cgi-bin/hgTables --post-data "hgsid=587035373_LgpTD7
 
 
 wget http://genome.ucsc.edu/cgi-bin/hgTables --post-data "hgsid=587035373_LgpTD7UIYEEaePMt5dz5SQF74Oie&jsh_pageVertPos=0&clade=mammal&org=Human&db=hg19&hgta_group=genes&hgta_track=refGene&hgta_table=refGene&hgta_regionType=genome&position=chr21%3A33031597-33041570&hgta_outputType=sequence&boolshad.sendToGalaxy=0&boolshad.sendToGreat=0&boolshad.sendToGenomeSpace=0&hgta_outFileName=hg19_refGene_table&hgta_compressType=none&hgta_doTopSubmit=get+output" -O temp
-wget "http://genome.ucsc.edu/cgi-bin/hgTables?hgsid=587035373_LgpTD7UIYEEaePMt5dz5SQF74Oie&hgta_geneSeqType=mRNA&hgta_doGenePredSequence=submit" -O sequence.fasta
+wget "http://genome.ucsc.edu/cgi-bin/hgTables?hgsid=587035373_LgpTD7UIYEEaePMt5dz5SQF74Oie&hgta_geneSeqType=mRNA&hgta_doGenePredSequence=submit" -O sequences.fasta
 rm temp
